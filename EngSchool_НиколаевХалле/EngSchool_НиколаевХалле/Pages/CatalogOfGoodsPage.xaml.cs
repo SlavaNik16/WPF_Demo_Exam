f8b78569efee1a62e71b21e996d8b5d05b92300c
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-02-22 18:11:13 +0300

    zip изменил на github vs

---
+++ EngSchool_НиколаевХалле/EngSchool_НиколаевХалле/Pages/CatalogOfGoodsPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using EngSchool_ВячеславХалле.Models;
+using EngSchool_МалышеваКоршикова.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EngSchool_ВячеславХалле.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CatalogOfGoodsPage.xaml
+    /// </summary>
+    public partial class CatalogOfGoodsPage : Page
+    {
+        int itemCount = 0;
+        public CatalogOfGoodsPage()
+        {
+            InitializeComponent();
+            using (var context = new BD26_EVNikolaevHalle())
+            {
+                var developers = context.Manufacturer.OrderBy(x => x.Name).ToList();
+                developers.Insert(0, new Manufacturer
+                {
+                    Name = "Все типы"
+                });
+                ComboDeveloper.ItemsSource = developers;
+                ComboDeveloper.SelectedIndex = 0;
+                LViewGoods.ItemsSource = context.Product.OrderBy(x => x.Name).ToList();
+                itemCount = LViewGoods.Items.Count;
+                TextBlockCount.Text = $"Результат запроcа: {itemCount} записей из {itemCount}";
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            using (var context = new BD26_EVNikolaevHalle())
+            if(Visibility == Visibility.Visible)
+            {
+                    context.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+                    LViewGoods.ItemsSource = context.Product.OrderBy(x => x.Name).ToList();
+            }
+        }
+
+        private void ComboDeveloper_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateData();
+        }
+        private void UpdateData()
+        {
+            using (var context = new BD26_EVNikolaevHalle())
+            {
+                var currentGoods = context.Product.OrderBy(x =>x.Name).ToList();
+                if (ComboDeveloper.SelectedIndex > 0)
+                {
+                    currentGoods = currentGoods.Where(p => p.ManufacturerID == (ComboDeveloper.SelectedItem as Manufacturer).ID).ToList();
+                }
+                currentGoods = currentGoods.Where(p => p.Name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+                if (ComboDeveloper.SelectedIndex >= 0)
+                {
+                    if(ComboSort.SelectedIndex == 0)
+                    {
+                        currentGoods = currentGoods.OrderBy(x => x.Name).ToList();
+                    }
+                    if (ComboSort.SelectedIndex == 1)
+                    {
+                        currentGoods = currentGoods.OrderByDescending(x => x.Name).ToList();
+                    }
+                }
+                LViewGoods.ItemsSource = currentGoods;
+                TextBlockCount.Text = $"Результат запроcа: {currentGoods.Count} записей из {itemCount}";
+            }
+        }
+
+    }
+}
---
+++ EngSchool_НиколаевХалле/EngSchool_НиколаевХалле/Pages/LoginWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using EngSchool_ВячеславХалле.Models;
+using EngSchool_МалышеваКоршикова.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EngSchool_ВячеславХалле.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult x = MessageBox.Show("Вы действительно хотите закрыть приложение ?", "Выйти", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if(x == MessageBoxResult.Cancel)
+                e.Cancel = true;
+
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new BD26_EVNikolaevHalle())
+                {
+                    List<User> users = context.User.ToList();
+                    User u = users.FirstOrDefault(p => p.Password ==TbPass.Password && p.Login == TbLogin.Text);
+                    if(u != null)
+                    {
+                        MainWindow mainWindow= new MainWindow();
+                        mainWindow.Owner= this;
+                        this.Hide();
+                        mainWindow.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный логин или пароль");
+                        TbLogin.Text = string.Empty;
+                        TbPass.Password= string.Empty;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
